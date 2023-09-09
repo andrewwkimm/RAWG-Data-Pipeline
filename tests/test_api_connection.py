32cdb47f65aac64d3e662e30ea8d00f1bfa67639
andrewwkimm <andrewkimka@gmail.com>
2023-09-09 22:50:55 +0000

    Fix failing test

--- tests/test_api_connection.py
+++ tests/test_api_connection.py
@@ -6,7 +6,7 @@ from src.utilities.rawg_api_utils import initialize_arguements
 def test_api_connection() -> None:
     """Tests response returns status code 200."""
     url, params, dates = initialize_arguements()
-    params[dates] = dates[0] + "," + dates[0]
+    params["dates"] = dates[0] + "," + dates[0]
 
     response = requests.get(url, params=params)
     assert (
